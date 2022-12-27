58006abd88fbd15684e31dd9d6e781a417bfdb66
mazzNeosoft <md.mashroor@neosoftmail.com>
2022-12-28 01:42:42 +0530

    Fix : Updated file structure

--- test/index.test.jsx
+++ test/index.test.jsx
@@ -1,10 +1,9 @@
+let { expect } = require('chai')
 import React from "react";
 import "@testing-library/jest-dom";
 import { fireEvent, render, screen } from "@testing-library/react";
-import singletonRouter, { useRouter } from 'next/router';
-import Home from '.';
+import Home from '../pages/index.js';
 import mockRouter from 'next-router-mock';
-
 jest.mock('next/router', () => require('next-router-mock'));
 
 beforeEach(() => {
@@ -13,47 +12,47 @@ beforeEach(() => {
 });
 describe("Should Search Fields initialsed", () => {
   it("Should Render Header", () => {
-    expect(screen.getByText("Logger Application")).toBeInTheDocument();
+    expect(screen.getByText("Logger Application")).to.exist;
   });
 
   it("Should Render Log ID Field", () => {
     let subTitle = screen.getAllByText(/Log Id/i);
-    expect(subTitle[0]).toBeInTheDocument();
+    expect(subTitle[0]).to.exist;
   });
 
   it("Should Render Application ID Field", () => {
     let subTitle = screen.getAllByText(/Application Id/i);
-    expect(subTitle[0]).toBeInTheDocument();
+    expect(subTitle[0]).to.exist;
   });
 
   it("Should Render Application Type Field", () => {
     let subTitle = screen.getAllByText(/Application Type/i);
-    expect(subTitle[0]).toBeInTheDocument();
+    expect(subTitle[0]).to.exist;
   });
 
   it("Should Render Action Type Field", () => {
     let subTitle = screen.getAllByText(/Application Type/i);
-    expect(subTitle[0]).toBeInTheDocument();
+    expect(subTitle[0]).to.exist;
   });
 
   it("Should Render From Datepicker Field", () => {
     let subTitle = screen.getAllByText(/From/i);
-    expect(subTitle[0]).toBeInTheDocument();
+    expect(subTitle[0]).to.exist;
   });
 
   it("Should Render To Datepicker Field", () => {
     let subTitle = screen.getAllByText(/To/i);
-    expect(subTitle[0]).toBeInTheDocument();
+    expect(subTitle[0]).to.exist;
   });
 
   it("Should Render Search Button", () => {
     let subTitle = screen.getAllByText(/search/i);
-    expect(subTitle[0]).toBeInTheDocument();
+    expect(subTitle[0]).to.exist;
   });
 
   it("Should Render Clear All", () => {
     let subTitle = screen.getAllByText(/clear all/i);
-    expect(subTitle[0]).toBeInTheDocument();
+    expect(subTitle[0]).to.exist;
   });
 });
 
